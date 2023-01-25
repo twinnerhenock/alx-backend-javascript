b7413051e8d8c32c3e4f22138607535c3e4f6526
twinnerhenock <demessiehe@gmail.com>
2023-01-25 14:22:14 +0000

    private attribute

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,22 +1,22 @@
 import Car from './10-car';
 
 export default class EVCar extends Car {
-	constructor(brand, motor, color, range) {
-		super(brand, motor, color);
-        	this.range = range;
-	}
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
 
-	get range() {
-		return this._range;
-	}
+  get range() {
+    return this._range;
+  }
 
-	set range(value) {
-	        this._range = value;
-	}
+  set range(value) {
+    this._range = value;
+  }
 
-	cloneCar() {
-		const Species = super.constructor[Symbol.species];
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
 
-		return new Species();
-	}
+    return new Species();
+  }
 }
