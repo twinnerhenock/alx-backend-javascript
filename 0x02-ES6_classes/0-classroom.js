c9ad349da440a995a207471cf2629f0a0dd313c2
twinnerhenock <demessiehe@gmail.com>
2023-01-25 13:04:18 +0000

    js class

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,14 @@
+/**
+ * Represents a class room.
+ */
+
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
